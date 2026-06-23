d4ca41ba68811790e21c8af47593a3fed213f63a
CTNOriginals <info@ctn-originals.com>
2026-06-23 23:14:22 +0200

    create(keymaps): findfiles for advanced file lists

--- lua/ctn/core/keymaps/init.lua
+++ lua/ctn/core/keymaps/init.lua
@@ -1,4 +1,5 @@
 require("ctn.core.keymaps.navigation")
 require("ctn.core.keymaps.editing")
 require("ctn.core.keymaps.buffers")
+require("ctn.core.keymaps.findfiles")
 require("ctn.core.keymaps.plugins")
