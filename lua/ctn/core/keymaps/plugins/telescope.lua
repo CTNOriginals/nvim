e1770ba0eedbdf18c434e4b38177ae381ecf6f4d
CTNOriginals <info@ctn-originals.com>
2026-06-23 17:49:12 +0200

    wip(keymaps): telescope find files with args

--- lua/ctn/core/keymaps/plugins/telescope.lua
+++ lua/ctn/core/keymaps/plugins/telescope.lua
@@ -28,3 +28,80 @@ end, { desc = "[s]earch [/] in Open Files" })
 vim.keymap.set("n", "<leader>sn", function()
 	builtin.find_files({ cwd = vim.fn.stdpath("config") })
 end, { desc = "[s]earch [n]eovim files" })
+
+---@class findFilesArgs
+---@field field string
+---@field value boolean
+---@field key string
+
+---@type findFilesArgs[]
+local findFilesDefaults = {
+	{ field = "follow", value = true, key = "f" },
+	{ field = "hidden", value = true, key = "h" },
+	{ field = "no_ignore", value = true, key = "i" },
+}
+
+---@type telescope.builtin.find_files.opts
+local findFilesOpts = {
+	bufnr = 0,
+	winnr = 0,
+}
+
+local findFileArgsSubmited = false
+
+vim.keymap.set("n", "<leader>sc", function()
+	findFileArgsSubmited = false
+end, { desc = "[s]earch [c]ustom" })
+
+vim.keymap.set("n", "<leader>sc<cr>", function()
+	findFileArgsSubmited = true
+end, { desc = "submit" })
+
+local findCustomKeys = "<leader>sc"
+---@param arg findFilesArgs
+local function executeFindFileArg(arg)
+	-- local triggerKeys = vim.api.nvim_replace_termcodes(findCustomKeys, false, false, true)
+	-- vim.api.nvim_feedkeys(vim.keycode(triggerKeys), "n", false)
+	-- vim.fn.feedkeys(vim.keycode(triggerKeys), "n")
+	findFilesOpts[arg.field] = arg.value
+	vim.defer_fn(function()
+		vim.fn.feedkeys(vim.keycode(findCustomKeys), "m")
+	end, 100)
+end
+
+for _, arg in ipairs(findFilesDefaults) do
+	-- vim.keymap.set("n", "<leader>sc" .. arg.key, "<leader>sc")
+
+	-- vim.keymap.set("n", "<leader>sc" .. arg.key, function()
+	-- 	executeFindFileArg(arg)
+	-- end, { desc = arg.field .. ": " .. tostring(arg.value) })
+	vim.keymap.set("n", "<leader>sc" .. arg.key, "<leader>sc", {
+		desc = arg.field .. ": " .. tostring(arg.value),
+		callback = function()
+			require("which-key").add()
+		end,
+	})
+end
+
+local function prototyper()
+	-- local path = getPath()
+	builtin.find_files({
+		hidden = true,
+		-- no_ignore = true,
+		no_ignore_parent = true,
+		follow = true,
+	})
+end
+
+vim.keymap.set("n", "<leader>sj", function()
+	prototyper()
+end, { desc = "[s]earch TEST" })
+
+-- vim.defer_fn(function()
+-- 	if vim.fn.getcwd() ~= "/home/ctn/.config/dotfiles" then
+-- 		return
+-- 	end
+-- 	prototyper()
+-- end, 100)
+
+-- print("here!")
